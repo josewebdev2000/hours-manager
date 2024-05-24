7bff231e97536f266e61931c7ffb32462e4b95a6
LordNyitter <ronnyrbenitez@gmail.com>
2024-05-24 18:39:20 -0400

    this is a test

---
+++ templates/main-header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HOURSMANAGER</title>
+    <link rel="stylesheet" href="assets\css\style.css">
+</head>
+<body>
+    <header>
+        <h1>HOURSMANAGER</h1>
+        <nav>
+            <a href="#">HOME</a>
+            <a href="#">ABOUT</a>
+            <a href="#">CONTACT</a>
+            <a href="#">PRIVACY</a>
+            <a href="#">LOGIN/REGISTER</a>
+        </nav>
+    </header>
+</body>
+</html>
