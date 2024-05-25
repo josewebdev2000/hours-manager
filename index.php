48feebbdc6c035ab1e6008ec1a190239f8994683
LordNyitter <ronnyrbenitez@gmail.com>
2024-05-25 00:34:45 -0400

    12:34AM

--- index.php
+++ index.php
@@ -1,18 +1,14 @@
 <?php require_once "templates/header.php"; ?>
 <?php require_once "templates/main-header.php"; ?>
 
-<main>
-    <div class="background-image">
-        <div class="content">
-            <div class="center-box">
-                <p>Are you the kind of person that always
-                forgets to track your working hours?<br>
-                Don't you think it'd be great to easily
-                manage and keep track of all your hours in one place?<br>
-                That place is HoursManager.<br>
-                What are you waiting for?</p>
-                <a href="#" class="start-here-button">Start Here</a>
-            </div>
+<main id="index-main">
+    <div class="content">
+        <div class="center-box">
+            <p>Are you the kind of person that always forgets to track your working hours?<br>
+            Don't you think it'd be great to easily manage and keep track of all your hours in one place?<br>
+            That place is HoursManager.<br>
+            What are you waiting for?</p>
+            <a href="#" class="start-here-button">Start Here</a>
         </div>
     </div>
 </main>
