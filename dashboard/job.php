956b61d2228c20676819ff58642c8fa35578951d
josewebdev2000 <josewebdev@proton.me>
2024-06-06 13:50:08 -0400

    Implemented GET Parameters for Job Page

--- dashboard/job.php
+++ dashboard/job.php
@@ -1,20 +1,112 @@
 <?php require_once "../templates/session-starter.php"; ?>
 <?php require_once "templates/dashboard-header.php"; ?>
 <?php require_once "../templates/header.php"; ?>
-<!--HTML CODE GOES HERE-->
-<div class="wrapper">
-    <!--IMPORT MAIN HEADER CODE-->
-    <?php require_once "templates/dashboard-main-header.php"; ?>
 
-    <!--IMPORT SIDEBAR CODE HERE-->
-    <?php require_once "templates/dashboard-sidebar.php"; ?>
+<!--This file will need to use GET parameter to conditionally render what is required-->
 
-    <!--IMPORT PRELOADER HERE-->
-    <?php require_once "templates/dashboard-preloader.php"; ?>
+<!--GET Params
+    action = ["view", "add", "update", "delete"]
+    id = Id of Job in DB for ["view", "update", "delete"]
+-->
 
-    <!--INCLUDE CODE TO SHOW/EDIT EACH JOB PROFILE PAGE INSIDE THE content-wrapper -->
-    <div class="content-wrapper">
-        I am The Job Page
+<!--
+    If GET Param action is not set, show error page
+    If GET param id is not set for ["view", "update", and "delete", show error page]
+-->
+<?php $allowed_actions = ["add", "view", "update", "delete"]; // Actions Job Page May Do ?>
+<?php $id_required_actions = ["view", "update", "delete"]; // DB Id of Job Entry ?>
+    <!--HTML CODE GOES HERE-->
+    <div class="wrapper">
+        <!--IMPORT MAIN HEADER CODE-->
+        <?php require_once "templates/dashboard-main-header.php"; ?>
+
+        <!--IMPORT SIDEBAR CODE HERE-->
+        <?php require_once "templates/dashboard-sidebar.php"; ?>
+
+        <!--IMPORT PRELOADER HERE-->
+        <?php require_once "templates/dashboard-preloader.php"; ?>
+
+        <!--INCLUDE CODE TO SHOW/EDIT EACH JOB PROFILE PAGE INSIDE THE content-wrapper -->
+        <div class="content-wrapper">
+            <!--If action is not set as get param, show action error-->
+            <?php if (!isset($_GET["action"]) || !in_array(strtolower($_GET["action"]), $allowed_actions)): ?>
+                <div class="content-header">
+                    <div class="container-fluid">
+                        <div class="row mb-2">
+                            <div class="col-sm-6">
+                                <h1 class="text-danger">Job Action Error</h1>
+                            </div>
+                            <div class="col-sm-6">
+                                <ol class="breadcrumb float-sm-right">
+                                    <li class="breadcrumb-item"><a href="<?=$websiteUrl?>dashboard/">Dashboard</a></li>
+                                    <li class="breadcrumb-item active">Job</li>
+                                </ol>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="content">
+                    <div class="error-page">
+                        <div class="headline text-danger">400</div>
+                        <div class="error-content">
+                            <h3 class="mb-4">
+                                <i class="fas fa-exclamation-triangle text-danger"></i>
+                                Bad Request
+                            </h3>
+                            <h4>Invalid Action Parameter</h4>
+                            <p>The <b>action</b> parameter was either not specified or invalid.</p>
+                        </div>
+                    </div>
+                </div>
+            <!--If Job Id Is Not Set for actions that need it, show id param error -->
+            <?php elseif (in_array(strtolower($_GET["action"]), $id_required_actions) && !isset($_GET["id"])): ?>
+                <div class="content-header">
+                    <div class="container-fluid">
+                        <div class="row mb-2">
+                            <div class="col-sm-6">
+                                <h1 class="text-danger">Job Id Error</h1>
+                            </div>
+                            <div class="col-sm-6">
+                                <ol class="breadcrumb float-sm-right">
+                                    <li class="breadcrumb-item"><a href="<?=$websiteUrl?>dashboard/">Dashboard</a></li>
+                                    <li class="breadcrumb-item active">Job</li>
+                                </ol>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="content">
+                    <div class="error-page">
+                        <div class="headline text-danger">400</div>
+                        <div class="error-content">
+                            <h3 class="mb-4">
+                                <i class="fas fa-exclamation-triangle text-danger"></i>
+                                Bad Request
+                            </h3>
+                            <h4>Invalid Id Parameter</h4>
+                            <p>The <b>id</b> parameter was either not specified or invalid.</p>
+                        </div>
+                    </div>
+                </div>
+            <!--Show Add New Page Here-->
+            <?php elseif ($_GET["action"] == "add"):?>
+                <div class="content-header">
+                    <div class="container-fluid">
+                        <div class="row mb-2">
+                            <div class="col-sm-6">
+                                <h1>Add New Job</h1>
+                            </div>
+                            <div class="col-sm-6">
+                                <ol class="breadcrumb float-sm-right">
+                                    <li class="breadcrumb-item"><a href="<?=$websiteUrl?>dashboard/">Dashboard</a></li>
+                                    <li class="breadcrumb-item active">Job</li>
+                                </ol>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="content"></div>
+            <?php endif; ?>
+        </div>
     </div>
-</div>
 <?php require_once "../templates/footer.php";?>
